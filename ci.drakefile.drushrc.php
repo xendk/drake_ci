521b61ff03d011213587836bc258763854b4abb7
Mads H. Danquah <mads@danquah.dk>
2013-08-28 17:10:12 +0200

    miiinor typo

--- ci.drakefile.drushrc.php
+++ ci.drakefile.drushrc.php
@@ -1428,7 +1428,7 @@ function drake_ci_behat_test($context) {
 
   // We'd like to use drush runserver instead, but in initial testing runserver
   // would cause core tests to fail on login, while this would not.
-  $cmd = 'php -t ' . $context['root'] . ' -S ' . $context['site-host'] . ' :' . $port . ' ' . dirname(__FILE__) . '/router.php';
+  $cmd = 'php -t ' . $context['root'] . ' -S ' . $context['site-host'] . ':' . $port . ' ' . dirname(__FILE__) . '/router.php';
   drush_log("Executing command " . $cmd, 'debug');
   $php_process = proc_open($cmd, $descriptorspec, $pipes);
   register_shutdown_function('_drake_ci_kill_process_shutdown', $php_process);
